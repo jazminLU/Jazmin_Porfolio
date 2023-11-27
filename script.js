ddad4bfd7a337ece85500149e53e355d2e03ccff
jazminLU <jluna2554@gmail.com>
2023-11-27 20:52:25 -0300

    nuevos archivos: .gitignore 	modificados:     index.html 	modificados:     script.js

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-/*==================== toggle icon navbar ====================*/
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+let language = 'en';
+
+let texts = {
+    'en': {
+        toggleIcon: 'Toggle Icon',
+        scrollSections: 'Scroll Sections',
+        stickyNavbar: 'Sticky Navbar',
+        revealScroll: 'Scroll Reveal',
+        typedTexts: ['QA Automation', 'Frontend Developer', 'Manual Tester']
+    },
+    'es': {
+        toggleIcon: 'Alternar Icono',
+        scrollSections: 'Desplazar Secciones',
+        stickyNavbar: 'Navbar Fija',
+        revealScroll: 'Mostrar al Desplazar',
+        typedTexts: ['QA Automatización', 'Desarrollador Frontend', 'Probador Manual']
+    }
+};
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
-/*==================== scroll sections active link ====================*/
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
-
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -18,44 +34,43 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-
-        };
+        }
     });
-/*==================== sticky navbar ====================*/
-let header = document.querySelector('header');
 
-header.classList.toggle('sticky', window.scrollY > 100);
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-/*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
-menuIcon.classList.remove('bx-x');
-navbar.classList.remove('active');
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
+
+// Resto del código
 
 /*==================== scroll reveal ====================*/
 ScrollReveal({
-     //reset: true,
-     distance: '80px',
-     duration:2000,
-     delay:200
-     });
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
 
-     ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-     ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-     ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-     ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
+/*==================== typed js ====================*/
+const typed = new Typed('.multiple-text', {
+    strings: texts[language].typedTexts,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
 
-     /*==================== typed js ====================*/
-     const typed = new Typed('.multiple-text', {
-        strings: ['QA Automation','Frontend Developer','Manual Tester'],
-        typeSpeed: 100,
-        backSpeed: 100,
-        backDelay: 1000,
-        loop:true
-     });
 
