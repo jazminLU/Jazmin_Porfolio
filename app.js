d527df7dea57a3dfc1b470dcd98e6a6f713b53c3
jazminLU <jluna2554@gmail.com>
2024-08-05 13:24:38 -0300

    modificados:     app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ particlesJS(
           },
           "move": {
             "enable": true,
-            "speed": 3,
+            "speed": 2,
             "direction": "none",
             "random": false,
             "straight": false,
