966b99b4514f38c99a544370d114ffd387a03ccc
jazminLU <jluna2554@gmail.com>
2024-08-05 13:15:42 -0300

    modificados:     app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ particlesJS(
           },
           "move": {
             "enable": true,
-            "speed": 6,
+            "speed": 4,
             "direction": "none",
             "random": false,
             "straight": false,
@@ -73,7 +73,7 @@ particlesJS(
           "events": {
             "onhover": {
               "enable": true,
-              "mode": "repulse"
+              "mode": "bubble"
             },
             "onclick": {
               "enable": true,
